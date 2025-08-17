e7f93d6d55549694d7b1e57a4d50fb4471eea75a
Alex Serediuc <alex.sr@yahoo.com>
2025-08-17 23:35:39 +0300

    Rearange

--- src/components/NameTitle.jsx
+++ src/components/NameTitle.jsx
@@ -1,10 +1,18 @@
 import { FullName } from "../constants/texts.js";
+import {
+    FaLinkedin
+} from "react-icons/fa";
 
 function DisplayName()
 {
     return (
         <div className='name-block'>
+
             <h1>{FullName}</h1>
+            <p>Software engineer</p>
+            <div  className="social-buttons">
+                <FaLinkedin  className="social-button"/>
+            </div>
         </div>
     );
 }
