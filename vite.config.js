8b6c87877702c7fb26d19d3964d2ce62d80b84b2
alxsrccnt-dot <alx.src.cnt@gmail.com>
2025-11-16 11:39:09 +0200

    Change vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/curriculum-vitae'
+  base: '/curriculum-vitae/',
+  build: {
+    outDir: 'docs'
+  }
 })
